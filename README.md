de3d1dfb520dfb48c81d3784e4bb241a2231e396
ravindra <chintakayalaravindra2004@gmail.com>
2023-12-06 15:05:43 +0530

    commiting this file

---
+++ README.md
@@ -0,0 +1,66 @@
+<p align="center">
+  <a href="https://leetcode-cn.com/leetbook/detail/illustration-of-algorithm/">
+    <img src="https://pic.leetcode-cn.com/1599187248-gDiWnC-image.png" width="490">
+  </a>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/LeetCode%20tests-75%20accepted-brightgreen">
+  <img src="https://img.shields.io/badge/Language-Python-teal">
+  <img src="https://img.shields.io/badge/Language-Java-orange">
+  <img src="https://img.shields.io/badge/Language-C++-blue">
+</p>
+
+[《图解算法数据结构》](https://leetcode-cn.com/leetbook/detail/illustration-of-algorithm/)是一本面向算法初学者和互联网求职者编写的 LeetBook 手册。
+
+- 图文详解 75 道题目，覆盖主要算法知识点。
+- 题目活跃于各大互联网公司招聘中，可使笔面试准备事半功倍。
+- 致力于行文深入浅出、图文搭配，提供简洁的 Python, Java, C++ 解题代码。
+
+> [!NOTE]  
+>
+> 本仓库包含“图解算法数据结构”、“Krahets 笔面试精选 88 题”和“剑指 Offer”的题解内容：
+> 
+> ```python
+> LeetCode-Book
+> ├── leetbook_ioa               # 《图解算法数据结构》题解和专栏文档、代码
+> ├── selected_coding_interview  # 《Krahets 笔面试精选 88 题》题解文档、代码
+> └── sword_for_offer            # 《剑指 Offer》题解文档、代码、刷题计划
+> ```
+
+> 若本 LeetBook 对您有所帮助，请在页面右上角点个 Star :star: 支持一下，谢谢！
+
+## 如何学习算法
+
+### 第一步：看入门书
+
+<p align="center" href="https://github.com/krahets/hello-algo">
+  <img src="https://www.hello-algo.com/index.assets/hello_algo_header.png" width="550">
+</p>
+
+[《Hello 算法》](https://github.com/krahets/hello-algo)：动画图解、一键运行的数据结构与算法教程。
+
+这本书旨在引导初学者探索数据结构与算法的知识地图，掌握刷题需要的前置知识与工具库。
+
+### 第二步：刷算法题
+
+推荐以下 LeetCode 题单（前两项 [@Krahets](https://leetcode.cn/u/jyd/) 题解全覆盖）：
+
+1. [Krahets 笔面试精选 88 题](https://leetcode.cn/studyplan/selected-coding-interview/)：从“剑指 Offer”和“热题 100”精选出的 88 道高频算法笔试题。
+2. [图解算法数据结构](https://leetcode-cn.com/leetbook/detail/illustration-of-algorithm/)（[Interview-75](https://leetcode.cn/studyplan/coding-interviews/)）：题目更贴近实际应用，相对“剑指 Offer”难度有所增加。
+3. [LeetCode 热题 100](https://leetcode.cn/studyplan/top-100-liked/)：力扣用户最喜爱的 100 道题。
+4. [面试经典 150 题](https://leetcode.cn/studyplan/top-interview-150/)：150 道经典面试力扣题。
+5. [LeetCode-75](https://leetcode.cn/studyplan/leetcode-75/)：精选面试必备的 75 道核心题目。
+
+对初学者的刷题建议：
+
+1. 建议每日刷 2~3 题。若能轻松完成，可以尝试增加至 5～8 题。
+2. 刷题的质量远重要于数量。务必确保你真正理解了每个题目的解法及背后的算法原理。
+3. 建议你按照目录顺序做题。如果感觉某题很难，可以先跳过，稍后再回顾挑战。
+4. 很多题目都有不止一种解法，请你注意比较和探讨各种方法的特点和适用情况。
+5. 如果你发现自己遗忘了题目解法，不必灰心。艾宾浩斯遗忘曲线指出，通常需要复习至少三次，才能真正掌握一个知识点。
+6. 行百里者半九十。坚持至关重要，加油，相信你可以做到！
+
+## License
+
+The texts, code and images in this repository are licensed under [CC BY-NC-SA-4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
---
+++ sword_for_offer/codes/java/sfo_20_a_string_representing_a_numeric_value_s1/sfo_20_a_string_representing_a_numeric_value_s1.java
@@ -0,0 +1,103 @@
+/*
+* File: sfo_20_a_string_representing_a_numeric_value_s1.java
+* Created Time: 2021-12-09
+* Author: Krahets (krahets@163.com)
+*/
+
+package sfo_20_a_string_representing_a_numeric_value_s1;
+
+import include.*;
+import java.util.*;
+
+// ===== Solution Code =====
+class Solution {
+    public boolean isNumber(String s) {
+        HashMap[] states = {
+                new HashMap<Character, Integer>() {
+                    {
+                        put(' ', 0);
+                        put('s', 1);
+                        put('d', 2);
+                        put('.', 4);
+                    }
+                }, // 0.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 2);
+                        put('.', 4);
+                    }
+                }, // 1.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 2);
+                        put('.', 3);
+                        put('e', 5);
+                        put(' ', 8);
+                    }
+                }, // 2.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 3);
+                        put('e', 5);
+                        put(' ', 8);
+                    }
+                }, // 3.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 3);
+                    }
+                }, // 4.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('s', 6);
+                        put('d', 7);
+                    }
+                }, // 5.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 7);
+                    }
+                }, // 6.
+                new HashMap<Character, Integer>() {
+                    {
+                        put('d', 7);
+                        put(' ', 8);
+                    }
+                }, // 7.
+                new HashMap<Character, Integer>() {
+                    {
+                        put(' ', 8);
+                    }
+                } // 8.
+        };
+        int p = 0;
+        char t;
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9')
+                t = 'd';
+            else if (c == '+' || c == '-')
+                t = 's';
+            else if (c == 'e' || c == 'E')
+                t = 'e';
+            else if (c == '.' || c == ' ')
+                t = c;
+            else
+                t = '?';
+            if (!states[p].containsKey(t))
+                return false;
+            p = (int) states[p].get(t);
+        }
+        return p == 2 || p == 3 || p == 7 || p == 8;
+    }
+}
+
+public class sfo_20_a_string_representing_a_numeric_value_s1 {
+    public static void main(String[] args) {
+        // ======= Test Case =======
+        String s = "    .1  ";
+        // ====== Driver Code ======
+        Solution slt = new Solution();
+        boolean res = slt.isNumber(s);
+        System.out.println(res);
+    }
+}
